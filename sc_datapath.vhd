7c949111ec9609d6b23185bf14dc0b4c4eb5658f
harleenksandhu <hsand052@uottawa.ca>
2025-06-19 17:15:31 -0400

    created project

---
+++ sc_datapath.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+library lpm;
+use lpm.lpm_components.all;
+
+
+entity sc_datapath is
+	port(GClk, GReset: in std_logic);
+
+end sc_datapath;
+
+
+architecture rtl of sc_datapath is
+
+end rtl;
+
+
+
