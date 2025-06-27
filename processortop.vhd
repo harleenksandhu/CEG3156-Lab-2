d2e4372cfdaf50fb86dbe68dfde7321749862f25
Milla168 <camillan168@gmail.com>
2025-06-27 17:35:46 -0400

    Revert "fixed compilation errors in processortop"
    This reverts commit 4e389adca3f45d3beb35047b9469dff06ea919c1.

--- processortop.vhd
+++ processortop.vhd
@@ -4,16 +4,11 @@ use ieee.std_logic_1164.all;
 entity processortop is
     port(ValueSelect: in std_logic_vector(2 downto 0);
         GClk, GReset: in std_logic;
-        MuxOut: out std_logic_vector(7 downto 0);
-        InstructionOut: out std_logic_vector(31 downto 0);
+        MuxOut: out in std_logic_vector(7 downto 0);
         BranchOut, MemWriteOut, RegWriteOut, ZeroOut: out std_logic);
 end processortop;
 
 architecture rtl of processortop is
-    signal int_pcout, int_aluresult, int_readdata1, int_readdata2, int_writedata, int_controlinfo : std_logic_vector(7 downto 0);
-    signal int_instruction : std_logic_vector(31 downto 0);
-    signal greset_b, int_zero, int_memwrite, int_regdst, int_alusrc, int_branch, int_jump, int_memtoreg, int_memread, int_regwrite : std_logic;
-    signal int_aluop, int_alufunc: std_logic_vector(1 downto 0);
 
 component nbit8to1mux
     GENERIC(n: integer:=8);
@@ -23,10 +18,10 @@ component nbit8to1mux
 end component;
 
 component sc_datapath
-	port(GClk, GReset, MemWrite, RegDst, ALUSrc, Branch, Jump, MemtoReg, MemRead, RegWrite: in std_logic;  -- from overall control block
-         ALUFunc: in std_logic_vector(1 downto 0);   -- from ALU control block
-         PCOut, ALUResult, ReadData1, ReadData2, WriteDate: out std_logic_vector(7 downto 0);    -- for MuxOut get control signals from control path
-         InstructionOut: out std_logic_vector(31 downto 0); 
+	port(GClk, GReset, MemWrite, RegDst, ALUSrc, Branch, Jump, MemtoReg, MemRead, RegWrite: in std_logic,  -- from overall control block
+         ALUFunc: in std_logic_vector(2 downto 0),   -- from ALU control block
+         PCOut, ALUResult, ReadData1, ReadData2, WriteDate: out std_logic_vector(7 downto 0),    -- for MuxOut get control signals from control path
+         InstructionOut: out std_logic_vector(31 downto 0), 
          ZeroOut: out std_logic);  
 end component;
 
@@ -44,8 +39,13 @@ end component;
 
 
 begin
+    signal int_pcout, int_aluresult, int_readdata1, int_readdata2, int_writedata : std_logic_vector(7 downto 0);
+    signal int_instruction : std_logic_vector(31 downto 0);
+    signal int_zero, int_memwrite, int_regdst, int_alusrc, int_branch, int_jump, int_memtoreg, int_memread, int_regwrite : std_logic;
+    signal int_aluop, int_alufunc: std_logic_vector(1 downto 0);
+    signal int_controlinfo : std_logic_vector(7 downto 0);
 
-control: main_control
+control: maincontrol
     port map(
         Instruction => int_instruction,
         MemWrite => int_memwrite,
@@ -59,7 +59,7 @@ control: main_control
         ALUOp => int_aluop
     );
 
-alucontrol_block: alucontrol
+alucontrol: alucontrol
     port map(
         ALUOp => int_aluop,
         Instruction => int_instruction,
